381c0ae129f42125e87bb85c18d8f98ac96ca8c1
Isabella <Isabella.salim@edu.omnia.fi>
2024-04-06 12:38:16 +0300

    added email validator

--- staff/staff.php
+++ staff/staff.php
@@ -12,18 +12,10 @@
 <?php require_once('../connection.php'); ?>
 <?php session_start();?>
 
+</div>
 <div class="login">
     <h1>Staff Login</h1>
     <br>
-    <form action="staff.php" method="post">
-        <label for="email"><i class="fa-solid fa-at"></i></label>
-        <input type="email" id="email" name="email" required placeholder ="Email"><br>
-        <label for="password"><i class="fas fa-lock"></i></label>
-        <input type="password" id="password" name="password" required placeholder ="Password"><br>
-        <button type="submit">Login</button>
-        <br>
-    </form>
-</div>
 
 <div class="container">
     <?php
@@ -34,40 +26,55 @@
         if (empty($email) || empty($password)) {
             echo '<p><b>Please fill in all fields</b></p>';
         } else {
-            $sql = "SELECT * FROM staff WHERE staff_email = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("s", $email);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            
-            if ($result->num_rows > 0) {
-                $row = $result->fetch_assoc();
-                $hashedPassword = $row['staff_password'];
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                echo '<p>Invalid email format</p>';
+            }else {
+                $sql = "SELECT * FROM staff WHERE staff_email = ?";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("s", $email);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                
+                if ($result->num_rows > 0) {
+                    $row = $result->fetch_assoc();
+                    $hashedPassword = $row['staff_password'];
 
-                if($email === $row['staff_email']){
-                    if (password_verify($password, $hashedPassword)) {
-                        $_SESSION['Staff_id'] = $row['Staff_id'];
-                        $_SESSION['staff_email'] = $email;
-                        header("Location: staff_index.php"); 
-                        exit();
-                    } else {
+                    if($email === $row['staff_email']){
+                        
+                        if (password_verify($password, $hashedPassword)) {
+                            $_SESSION['Staff_id'] = $row['Staff_id'];
+                            $_SESSION['staff_email'] = $email;
+                            header("Location: staff_index.php"); 
+                            exit();
+                        } else {
+                            echo '<br>';
+                            echo '<p><b>Invalid email or password </b></p>';
+                        }
+                    }else {
                         echo '<br>';
                         echo '<p><b>Invalid email or password </b></p>';
                     }
-                }else {
+                
+                } else {
                     echo '<br>';
-                    echo '<p><b>Invalid email or password </b></p>';
+                    echo '<p><b>Invalid email or password</b></p>';
                 }
-             
-            } else {
-                echo '<br>';
-                echo '<p><b>Invalid email or password</b></p>';
+                $stmt->close();
             }
-            $stmt->close();
+           
         }
         $conn->close();
     }
     ?>
+
+    <form action="staff.php" method="post">
+        <label for="email"><i class="fa-solid fa-at"></i></label>
+        <input type="email" id="email" name="email" required placeholder ="Email"><br>
+        <label for="password"><i class="fas fa-lock"></i></label>
+        <input type="password" id="password" name="password" required placeholder ="Password"><br>
+        <button type="submit">Login</button>
+        <br>
+    </form>
 </div>
 </body>
 </html>
